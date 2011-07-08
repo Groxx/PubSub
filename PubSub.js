51054da198111d382996c98d6d0efc670e09fd34
Steven Littiebrant <imgroxx@gmail.com>
2011-07-08 17:47:14 -0500

    improved license info

--- PubSub.js
+++ PubSub.js
@@ -1,4 +1,27 @@
-/** Copyright 2011 by Steven Littiebrant under MIT license - changes appreciated but not required: https://github.com/Groxx/PubSub **/
+/**
+Copyright 2011 by Steven Littiebrant - changes and communication about use 
+appreciated but not required: https://github.com/Groxx/PubSub
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.  Minified files may 
+reduce this license information to the first two lines at the top of the 
+LICENSE file.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ **/
 ;
 // Creates a new subscription object.  If passed, attaches to the passed object; otherwise, assumes it was called with "new" and binds to `this`.
 // If passed, unique defines the internal unique subscription list's storage name.  By default it is "_sub".
